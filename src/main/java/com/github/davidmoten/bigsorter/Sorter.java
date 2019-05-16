0af291d77edbe79f5023641bad113d376fd8aa09
Dave Moten <davidmoten@gmail.com>
2019-05-17 08:04:30 +1000

    fix imports

--- src/main/java/com/github/davidmoten/bigsorter/Sorter.java
+++ src/main/java/com/github/davidmoten/bigsorter/Sorter.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.stream.Collectors;
 
 import com.github.davidmoten.guavamini.Preconditions;
 
