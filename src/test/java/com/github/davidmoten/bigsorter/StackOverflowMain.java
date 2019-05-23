88d440f39a9562f8dd7bd1505bcc53211ab408c7
Dave Moten <davidmoten@gmail.com>
2019-05-24 08:23:07 +1000

    add example for stackoverflow big csv sorting question

---
+++ src/test/java/com/github/davidmoten/bigsorter/StackOverflowMain.java
@@ -0,0 +1,31 @@
+package com.github.davidmoten.bigsorter;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Random;
+
+public class StackOverflowMain {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        long t = System.currentTimeMillis();
+        File input = new File("target/input.csv");
+        try (PrintStream p = new PrintStream(
+                new BufferedOutputStream(new FileOutputStream(input)))) {
+            Random r = new Random();
+            for (int i = 0; i < 12_000_000; i++) {
+                for (int j = 0; j < 37; j++) {
+                    if (j > 0) {
+                        p.print(",");
+                    }
+                    p.print(r.nextInt() % 100000);
+                }
+                p.println();
+            }
+        }
+        System.out.println((System.currentTimeMillis() - t) / 1000.0 + "s");
+    }
+
+}
