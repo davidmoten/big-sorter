b417ead72fd43be8345630c2e023d94c7af1d5c9
David Moten <davidmoten@gmail.com>
2019-06-04 17:48:38 +1000

    add PositionRange

--- src/test/java/com/github/davidmoten/bigsorter/Index.java
+++ src/test/java/com/github/davidmoten/bigsorter/Index.java
@@ -1,9 +1,14 @@
 package com.github.davidmoten.bigsorter;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeMap;
 
+import org.davidmoten.hilbert.HilbertCurve;
 import org.davidmoten.hilbert.Range;
+import org.davidmoten.hilbert.SmallHilbertCurve;
 
 public class Index {
 
@@ -12,25 +17,55 @@ public class Index {
     private final double[] mins;
     private final double[] maxes;
     private final TreeMap<Integer, Long> indexPositions;
+    private final SmallHilbertCurve hc;
 
-    public Index(int bits, int dimensions, double[] mins, double[] maxes,
-            TreeMap<Integer, Long> indexPositions) {
+    public Index(int bits, int dimensions, double[] mins, double[] maxes, TreeMap<Integer, Long> indexPositions) {
         this.bits = bits;
         this.dimensions = dimensions;
         this.mins = mins;
         this.maxes = maxes;
         this.indexPositions = indexPositions;
+        this.hc = HilbertCurve.small().bits(bits).dimensions(dimensions);
     }
 
     /**
      * Fits the desired ranges to the effective querying ranges according to the
      * known index positions.
      * 
-     * @param ranges list of ranges in ascending order
+     * @param ranges
+     *            list of ranges in ascending order
      * @return querying ranges based on known index positions
      */
-    public List<Range> fit(List<Range> ranges) {
-        return null;
+    public List<PositionRange> fit(List<Range> ranges) {
+        List<PositionRange> list = new ArrayList<>();
+        for (Range range : ranges) {
+            Long startPosition = indexPositions.floorEntry((int) range.low()).getValue();
+            if (startPosition == null) {
+                startPosition = indexPositions.firstEntry().getValue();
+            }
+            Long endPosition = indexPositions.ceilingEntry((int) range.high()).getValue();
+            if (endPosition == null) {
+                endPosition = indexPositions.lastEntry().getValue();
+            }
+            list.add(new PositionRange(Collections.singletonList(range), startPosition, endPosition));
+        }
+        return simplify(list);
+    }
+
+    private static List<PositionRange> simplify(List<PositionRange> positionRanges) {
+        LinkedList<PositionRange> list = new LinkedList<>();
+        for (PositionRange p : positionRanges) {
+            if (list.isEmpty()) {
+                list.add(p);
+            } else {
+                PositionRange last = list.getLast();
+                if (p.overlapsPositionWith(last)) {
+                    list.pollLast();
+                    list.offer(last.join(p));
+                }
+            }
+        }
+        return list;
     }
 
 }
---
+++ src/test/java/com/github/davidmoten/bigsorter/PositionRange.java
@@ -0,0 +1,57 @@
+package com.github.davidmoten.bigsorter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.davidmoten.hilbert.Range;
+
+import com.github.davidmoten.guavamini.Preconditions;
+
+public final class PositionRange {
+
+    // hilbert curve index ranges covered by this position range
+    private final List<Range> ranges;
+
+    // highest known position with index less than or equal to lowIndex or the
+    // lowest known position if nothing lower
+    private final long floorPosition;
+
+    // lowest known position with index less than or equal to highIndex or the
+    // highest known position if nothing higher
+    private final long ceilingPosition;
+
+    public PositionRange(List<Range> ranges, long floorPosition, long ceilingPosition) {
+        Preconditions.checkArgument(!ranges.isEmpty());
+        this.ranges = ranges;
+        this.floorPosition = floorPosition;
+        this.ceilingPosition = ceilingPosition;
+    }
+
+    public List<Range> ranges() {
+        return ranges;
+    }
+
+    public long floorPosition() {
+        return floorPosition;
+    }
+
+    public long ceilingPosition() {
+        return ceilingPosition;
+    }
+
+    public boolean overlapsPositionWith(PositionRange last) {
+        return (floorPosition >= last.floorPosition && floorPosition <= last.ceilingPosition)
+                || (ceilingPosition <= last.ceilingPosition && ceilingPosition >= last.floorPosition);
+    }
+
+    public PositionRange join(PositionRange other) {
+        Preconditions.checkArgument(other.ranges.get(0).low() >= this.ranges.get(ranges.size() - 1).high());
+        List<Range> ranges = new ArrayList<>();
+        ranges.addAll(this.ranges);
+        ranges.addAll(other.ranges);
+        return new PositionRange(ranges, //
+                Math.min(floorPosition, other.floorPosition), //
+                Math.max(ceilingPosition, other.ceilingPosition));
+    }
+
+}
