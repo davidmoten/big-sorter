c102d1dc9423df94d17c7633b40665f5fb9be49d
David Moten <davidmoten@gmail.com>
2019-07-26 11:37:00 +1000

    use SequenceInputStream in test

--- src/test/java/com/github/davidmoten/bigsorter/JsonSorterMain.java
+++ src/test/java/com/github/davidmoten/bigsorter/JsonSorterMain.java
@@ -4,39 +4,41 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.SequenceInputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.Base64;
+import java.util.Enumeration;
 import java.util.Iterator;
 
 public class JsonSorterMain {
 
-    private static final long N = 10000000;
+    private static final long N = 1000;
 
     public static void main(String[] args) throws IOException {
 
         Iterator<String> it = new Iterator<String>() {
 
-            long count = N;
+            long remaining = N;
             SecureRandom random = new SecureRandom();
             byte[] bytes = new byte[16];
 
             @Override
             public boolean hasNext() {
-                return count > 0;
+                return remaining > 0;
             }
 
             @Override
             public String next() {
-                count--;
+                remaining--;
                 StringBuilder s = new StringBuilder();
-                if (count == N - 1) {
+                if (remaining == N - 1) {
                     s.append("[");
                 }
                 random.nextBytes(bytes);
                 s.append("{\"name\":\"" + Base64.getEncoder().encodeToString(bytes) + "\", \"age\":"
                         + Math.abs(random.nextInt()) % 100 + "}");
-                if (count == 0) {
+                if (remaining == 0) {
                     s.append("]");
                 } else {
                     s.append(",");
@@ -44,25 +46,37 @@ public class JsonSorterMain {
                 return s.toString();
             }
         };
-        InputStream in = new InputStream() {
-
-            ByteArrayInputStream b = new ByteArrayInputStream(new byte[0]);
+        // InputStream in = new InputStream() {
+        //
+        // ByteArrayInputStream b = new ByteArrayInputStream(new byte[0]);
+        //
+        // @Override
+        // public int read() throws IOException {
+        // int x;
+        // while ((x = b.read()) == -1) {
+        // if (it.hasNext()) {
+        // String next = it.next();
+        // b = new ByteArrayInputStream(next.getBytes(StandardCharsets.UTF_8));
+        // } else {
+        // return -1;
+        // }
+        // }
+        // return x;
+        // }
+        //
+        // };
+        InputStream in = new SequenceInputStream(new Enumeration<InputStream>() {
 
             @Override
-            public int read() throws IOException {
-                int x;
-                while ((x = b.read()) == -1) {
-                    if (it.hasNext()) {
-                        String next = it.next();
-                        b = new ByteArrayInputStream(next.getBytes(StandardCharsets.UTF_8));
-                    } else {
-                        return -1;
-                    }
-                }
-                return x;
+            public boolean hasMoreElements() {
+                return it.hasNext();
             }
 
-        };
+            @Override
+            public InputStream nextElement() {
+                return new ByteArrayInputStream(it.next().getBytes(StandardCharsets.UTF_8));
+            }
+        });
 
         Sorter.serializer(Serializer.jsonArray()) //
                 .comparator((x, y) -> x.get("name").asText().compareTo(y.get("name").asText())) //
@@ -70,6 +84,7 @@ public class JsonSorterMain {
                 .output(new File("target/output.json")) //
                 .loggerStdOut() //
                 .sort();
+
     }
 
 }
