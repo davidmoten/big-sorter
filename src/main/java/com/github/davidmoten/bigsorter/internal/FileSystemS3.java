6f798e989f8f7281a5af6fe4f6b4e2796cfc7339
Dave Moten <davidmoten@gmail.com>
2022-01-09 21:19:28 +1100

    s3 dev

---
+++ src/main/java/com/github/davidmoten/bigsorter/internal/FileSystemS3.java
@@ -0,0 +1,88 @@
+package com.github.davidmoten.bigsorter.internal;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import com.github.davidmoten.aws.lw.client.Client;
+import com.github.davidmoten.aws.lw.client.HttpMethod;
+import com.github.davidmoten.aws.lw.client.Multipart;
+import com.github.davidmoten.aws.lw.client.ResponseInputStream;
+import com.github.davidmoten.aws.lw.client.xml.builder.Xml;
+import com.github.davidmoten.bigsorter.FileSystem;
+
+public final class FileSystemS3 implements FileSystem {
+
+	private final Client s3;
+	private final int partSize;
+	private final int partTimeoutMs;
+	private final String region;
+
+	public FileSystemS3(Client s3, int partSize, int partTimeoutMs, String region) {
+		this.s3 = s3;
+		this.partSize = partSize;
+		this.partTimeoutMs = partTimeoutMs;
+		this.region = region;
+	}
+
+	@Override
+	public File nextTempFile(File directory) throws IOException {
+		return new File(directory, "big-sorter-" + UUID.randomUUID().toString().replace("-", ""));
+	}
+
+	@Override
+	public OutputStream outputStream(File file) throws IOException {
+		return Multipart //
+				.s3(s3)//
+				.bucket(file.getParentFile().getName()).key(file.getName()) //
+				.partSize(partSize) //
+				.partTimeout(partTimeoutMs, TimeUnit.MILLISECONDS) //
+				.outputStream();
+	}
+
+	@Override
+	public InputStream inputStream(File file) throws IOException {
+		ResponseInputStream r = s3 //
+				.path(file.getParentFile().getName(), file.getName()) //
+				.responseInputStream();
+		if (r.statusCode() / 100 == 2) {
+			return r;
+		} else {
+			throw new IOException("statusCode=" + r.statusCode());
+		}
+	}
+
+	@Override
+	public void move(File a, File b) throws IOException {
+		try (InputStream in = inputStream(a); OutputStream out = outputStream(b)) {
+			copy(in, out);
+		}
+	}
+
+	private static void copy(InputStream in, OutputStream out) throws IOException {
+		byte[] buffer = new byte[8192];
+		int n;
+		while ((n = in.read(buffer)) != -1) {
+			out.write(buffer, 0, n);
+		}
+	}
+
+	@Override
+	public void delete(File file) throws IOException {
+		s3.path(file.getParentFile().getName(), file.getName()) //
+				.method(HttpMethod.DELETE) //
+				.execute();
+	}
+
+	@Override
+	public void mkdirs(File directory) {
+		String xml = Xml.create("CreateBucketConfiguration").a("xmlns", "http://s3.amazonaws.com/doc/2006-03-01/")
+				.e("LocationConstraint") //
+				.content(region).toString();
+		s3.path(directory.getName()).method(HttpMethod.PUT).requestBody(xml).execute();
+	}
+
+}
