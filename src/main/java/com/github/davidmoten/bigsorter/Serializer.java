c9f70615ebbcf97bf9f8f1ea9218911d368cc81e
Dave Moten <davidmoten@gmail.com>
2022-01-11 17:15:19 +1100

    dev

--- src/main/java/com/github/davidmoten/bigsorter/Serializer.java
+++ src/main/java/com/github/davidmoten/bigsorter/Serializer.java
@@ -2,7 +2,6 @@ package com.github.davidmoten.bigsorter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
